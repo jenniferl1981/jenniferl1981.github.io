d3c62724e847634684dc115300683ce05d97c40e
jenniferl1981 <91750444+jenniferl1981@users.noreply.github.com>
2021-10-01 12:53:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Li, Jennifer"; // update the part BETWEEN the "quotes"
+let userName = "jenniferl1981"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
